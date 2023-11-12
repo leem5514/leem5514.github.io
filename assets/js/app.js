c8a7ff3d972393312f98d21eac044723adffd582
leem5514 <ikaros0569@gmail.com>
2023-11-12 19:20:31 +0900

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,40 @@
-$(document).ready(function(){
-  alert("hello world");
-if(window.location.href=="http://meliodus.github.io/uno-dbyll/")
-{
-$("#sidebar").css({width:'100%'});
-$("#btnblog").click(function(){
-$("#sidebar").animate({width:'33.3333%'},'slow');
-});
-}
-});
+(function($) {
+
+ function init() {
+    /* Sidebar height set */
+    $sidebarStyles = $('.sidebar').attr('style') || "";
+    $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
+    $('.sidebar').attr('style', $sidebarStyles);
+
+    /* Secondary contact links */
+    var $scontacts = $('#contact-list-secondary');
+    var $contactList = $('#contact-list');
+
+    $scontacts.hide();
+    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
+    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
+
+    /**
+     * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
+     */
+    function activateTab() {
+      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
+        var hash = decodeURIComponent(window.location.hash);
+        if(hash)
+          $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
+        else
+          $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
+      }
+    }
+
+    // watch hash change and activate relevant tab
+    $(window).on('hashchange', activateTab);
+
+    // initial activation
+    activateTab();
+  };
+
+  // run init on document ready
+  $(document).ready(init);
+
+})(jQuery);
